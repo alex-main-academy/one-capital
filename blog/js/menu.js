a82b06a11c1b839c7822037977baf6c4379d344a
alex-main-academy <oleksandrilnytskyi@gmail.com>
2023-05-05 01:42:00 +0200

    add-blog-page

---
+++ blog/js/menu.js
@@ -0,0 +1,21 @@
+const openMenuButton = document.querySelector(".js-menu-open");
+const closeMenuButton = document.querySelector(".js-menu-close");
+const menuElement = document.querySelector(".js-menu");
+
+openMenuButton.addEventListener("click", () => {
+  menuElement.style.cssText = `
+        opacity: 1;
+        transform: translate(0);
+        z-index: 1000;
+    `;
+  document.body.style.overflowY = "hidden";
+});
+
+closeMenuButton.addEventListener("click", () => {
+  menuElement.style.cssText = `
+          opacity: 0;
+          transform: translate(-100%);
+      `;
+
+  document.body.style.overflowY = "auto";
+});
