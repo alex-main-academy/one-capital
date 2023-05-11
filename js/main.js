a562a0c3489c975140d94db9ebeb9be0219e604c
alex-main-academy <oleksandrilnytskyi@gmail.com>
2023-05-11 15:47:16 +0300

    animation-mob

--- js/main.js
+++ js/main.js
@@ -29,56 +29,150 @@ const priorityTextFourth = document.querySelector(".priority__text_fourth");
 window.addEventListener("scroll", () => {
   const scrollPosition = window.scrollY;
 
-  if (scrollPosition < 650) {
-    priorityNumber.innerHTML = "01";
-  } else if (scrollPosition > 651 && scrollPosition < 820) {
-    priorityNumber.innerHTML = "02";
-  } else if (scrollPosition > 821 && scrollPosition < 950) {
-    priorityNumber.innerHTML = "03";
-  } else if (scrollPosition > 951) {
-    priorityNumber.innerHTML = "04";
-  }
+  if (window.outerWidth >= 1110) {
+    if (scrollPosition < 650) {
+      priorityNumber.innerHTML = "01";
+    } else if (scrollPosition > 651 && scrollPosition < 820) {
+      priorityNumber.innerHTML = "02";
+    } else if (scrollPosition > 821 && scrollPosition < 950) {
+      priorityNumber.innerHTML = "03";
+    } else if (scrollPosition > 951) {
+      priorityNumber.innerHTML = "04";
+    }
 
-  if (scrollPosition < 100) {
-    priorityTextFirst.style.cssText = `
-      opacity: 0;
-      transform: translate(100);
-    `;
-    priorityTextSecond.style.cssText = `
-      opacity: 0;
-      transform: translate(100);
-    `;
-    priorityTextThird.style.cssText = `
-      opacity: 0;
-      transform: translate(100);
-    `;
-    priorityTextFourth.style.cssText = `
-      opacity: 0;
-      transform: translate(100);
-    `;
-  }
+    if (scrollPosition < 100) {
+      priorityTextFirst.style.cssText = `
+        opacity: 0;
+        transform: translate(30);
+      `;
+      priorityTextSecond.style.cssText = `
+        opacity: 0;
+        transform: translate(30);
+      `;
+      priorityTextThird.style.cssText = `
+        opacity: 0;
+        transform: translate(30);
+      `;
+      priorityTextFourth.style.cssText = `
+        opacity: 0;
+        transform: translate(30);
+      `;
+    }
 
-  if (scrollPosition > 300 && scrollPosition < 399) {
-    priorityTextFirst.style.cssText = `
-      opacity: 1;
-      transform: translate(0);
-    `;
-  } else if (scrollPosition > 500 && scrollPosition < 600) {
-    priorityTextSecond.style.cssText = `
-      opacity: 1;
-      transform: translate(0);
-    `;
-  } else if (scrollPosition > 651 && scrollPosition < 750) {
-    priorityTextThird.style.cssText = `
-      opacity: 1;
-      transform: translate(0);
-    `;
-  } else if (scrollPosition > 851) {
-    priorityTextFourth.style.cssText = `
-      opacity: 1;
-      transform: translate(0);
-    `;
-  }
+    if (scrollPosition > 300 && scrollPosition < 399) {
+      priorityTextFirst.style.cssText = `
+        opacity: 1;
+        transform: translate(0);
+      `;
+    } else if (scrollPosition > 500 && scrollPosition < 600) {
+      priorityTextSecond.style.cssText = `
+        opacity: 1;
+        transform: translate(0);
+      `;
+    } else if (scrollPosition > 651 && scrollPosition < 750) {
+      priorityTextThird.style.cssText = `
+        opacity: 1;
+        transform: translate(0);
+      `;
+    } else if (scrollPosition > 851) {
+      priorityTextFourth.style.cssText = `
+        opacity: 1;
+        transform: translate(0);
+      `;
+    }
+  } else if (window.outerWidth >= 768 && window.outerWidth < 1110) {
+    if (scrollPosition < 600) {
+      priorityNumber.innerHTML = "01";
+    } else if (scrollPosition > 600 && scrollPosition < 750) {
+      priorityNumber.innerHTML = "02";
+    } else if (scrollPosition > 751 && scrollPosition < 900) {
+      priorityNumber.innerHTML = "03";
+    } else if (scrollPosition > 901) {
+      priorityNumber.innerHTML = "04";
+    }
 
-  console.log(scrollPosition);
+    if (scrollPosition < 100) {
+      priorityTextFirst.style.cssText = `
+        opacity: 0;
+        transform: translate(30);
+      `;
+      priorityTextSecond.style.cssText = `
+        opacity: 0;
+        transform: translate(30);
+      `;
+      priorityTextThird.style.cssText = `
+        opacity: 0;
+        transform: translate(30);
+      `;
+      priorityTextFourth.style.cssText = `
+        opacity: 0;
+        transform: translate(30);
+      `;
+    }
+
+    if (scrollPosition > 300 && scrollPosition < 399) {
+      priorityTextFirst.style.cssText = `
+        opacity: 1;
+        transform: translate(0);
+      `;
+    } else if (scrollPosition > 500 && scrollPosition < 600) {
+      priorityTextSecond.style.cssText = `
+        opacity: 1;
+        transform: translate(0);
+      `;
+    } else if (scrollPosition > 651 && scrollPosition < 750) {
+      priorityTextThird.style.cssText = `
+        opacity: 1;
+        transform: translate(0);
+      `;
+    } else if (scrollPosition > 851) {
+      priorityTextFourth.style.cssText = `
+        opacity: 1;
+        transform: translate(0);
+      `;
+    }
+  } else if (window.outerWidth <= 767) {
+    if (scrollPosition < 1170) {
+      priorityNumber.innerHTML = "01";
+    } else if (scrollPosition > 1171 && scrollPosition < 1300) {
+      priorityNumber.innerHTML = "02";
+    } else if (scrollPosition > 1301 && scrollPosition < 1420) {
+      priorityNumber.innerHTML = "03";
+    } else if (scrollPosition > 1421) {
+      priorityNumber.innerHTML = "04";
+    }
+
+    if (scrollPosition < 100) {
+      priorityTextFirst.style.cssText = `
+        opacity: 0;
+      `;
+      priorityTextSecond.style.cssText = `
+        opacity: 0;
+      `;
+      priorityTextThird.style.cssText = `
+        opacity: 0;
+      `;
+      priorityTextFourth.style.cssText = `
+        opacity: 0;
+      `;
+    }
+
+    if (scrollPosition > 800 && scrollPosition < 1100) {
+      priorityTextFirst.style.cssText = `
+        opacity: 1;
+      `;
+    } else if (scrollPosition > 1101 && scrollPosition < 1250) {
+      priorityTextSecond.style.cssText = `
+        opacity: 1;
+      `;
+    } else if (scrollPosition > 1251 && scrollPosition < 1400) {
+      priorityTextThird.style.cssText = `
+        opacity: 1;
+      `;
+    } else if (scrollPosition > 1401) {
+      priorityTextFourth.style.cssText = `
+        opacity: 1;
+      `;
+    }
+  }
 });
